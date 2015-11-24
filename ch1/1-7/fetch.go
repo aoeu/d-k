ff2de088f586717d3e150b5b76801576e170e9ac
aoeu <travis@mcdem.us>
2015-11-24 09:05:56 -0500

    Specify http protocols on args without (Ex 1.8)

--- ch1/1-7/fetch.go
+++ ch1/1-7/fetch.go
@@ -4,11 +4,25 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	urls "net/url"
 	"os"
+	"regexp"
 )
+
+var protoRegexp = regexp.MustCompile(`^http[s]?://[a-z]+`)
 
 func main() {
 	for _, url := range os.Args[1:] {
+		if !protoRegexp.Match([]byte(url)) {
+			url = fmt.Sprintf(`http://%s`, url)
+			if !protoRegexp.Match([]byte(url)) {
+				fmt.Fprintf(os.Stderr, "fetch: possibly invalid URL: %s\n", url)
+				os.Exit(1)
+			}
+		}
+		if _, err := urls.Parse(url); err != nil {
+			fmt.Fprintf(os.Stderr, "fetch; invalid URL %s: %v\n", url, err)
+		}
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
