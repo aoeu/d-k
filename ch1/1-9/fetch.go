faa4307d84b5c70ffb8ec5a5538160937a74ee93
aoeu <travis@mcdem.us>
2015-11-24 16:27:37 -0500

    Print HTTP status code for exercise 1.9

---
+++ ch1/1-9/fetch.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	urls "net/url"
+	"os"
+	"regexp"
+)
+
+var protoRegexp = regexp.MustCompile(`^http[s]?://[a-z]+`)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		if !protoRegexp.Match([]byte(url)) {
+			url = fmt.Sprintf(`http://%s`, url)
+			if !protoRegexp.Match([]byte(url)) {
+				fmt.Fprintf(os.Stderr, "fetch: possibly invalid URL: %s\n", url)
+				os.Exit(1)
+			}
+		}
+		if _, err := urls.Parse(url); err != nil {
+			fmt.Fprintf(os.Stderr, "fetch; invalid URL %s: %v\n", url, err)
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		defer resp.Body.Close()
+		fmt.Fprintf(os.Stdout, "The HTTP response's status code is: %v\n", resp.Status)
+		_, err = io.Copy(os.Stdout, resp.Body)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+	}
+}
